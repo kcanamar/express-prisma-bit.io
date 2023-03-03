194b0787b9a4ec7a75e3e29eee28396d22f2fe26
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-03-03 10:09:11 -0800

    complete CRUD connected to bit.io

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
-CREATE TABLE "todos_todo" (
-    "id" BIGSERIAL NOT NULL,
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
     "subject" VARCHAR(100) NOT NULL,
     "details" VARCHAR(100) NOT NULL,
 
-    CONSTRAINT "todos_todo_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
 );
 
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 
+// Prisma Setup
+////////////////////////////
+
+// Import the PrismaClient constructor from the @prisma/client node module
+const { PrismaClient } = require('@prisma/client')
+// Instantiate PrismaClient
+const prisma = new PrismaClient()
+
 const app = express()
 const { PORT = 3000 } = process.env
 
@@ -11,23 +19,77 @@ app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.json({message: "Hello"})
+
+
+app.get("/", async (req, res) => {
+    // find all todos
+    const todos = await prisma.Todo.findMany()
+
+    res.json(todos) 
 })
-app.get("/:id", (req, res) => {
-    res.json({ id: req.params.id })
+app.get("/:id", async (req, res) => {
+    // get the id off request
+    // ! Note the data type of the identifier must match the schema
+    const id = Number(req.params.id)
+    // find on todo
+    const todo = await prisma.Todo.findUnique({
+        // SQL WHERE QUERY
+        where: { id }
+    })
+
+    res.json(todo)
 })
 
-app.post("/", (req, res) => {
-    res.json({body: req.body})
+app.post("/", async (req, res) => {
+    // get parts from req.body
+    const { subject, details } = req.body
+
+    // create a todo
+    const todo = await prisma.Todo.create({
+        // Data in schema to create
+        data: {
+            subject,
+            details
+        }
+    })
+
+    res.json(todo)
 })
 
-app.put("/:id", (req, res) => {
-    res.json({ body: req.body, id: req.params.id })
+app.put("/:id", async (req, res) => {
+    // get the id off request
+    // ! Note the data type of the identifier must match the schema
+    const id = Number(req.params.id)
+
+    // get data from request body
+    const { subject, details } = req.body
+
+    // update 
+    const todo = await prisma.Todo.update({
+        // SQL WHERE QUERY
+        where: { id },
+        // Update with data
+        data: {
+            subject,
+            details
+        }
+
+    })
+    res.json(todo)
 })
 
-app.delete("/:id", (req, res) => {
-    res.json({ id: req.params.id})
+app.delete("/:id", async (req, res) => {
+    // get the id off request
+    // ! Note the data type of the identifier must match the schema
+    const id = Number(req.params.id)
+
+    // delete 
+    const todo = await prisma.Todo.delete({
+        where: { id }
+    })
+
+
+    res.json(todo)
 })
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
